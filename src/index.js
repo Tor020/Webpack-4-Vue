dd694342378cc8524b765f9ac4d28a07c66e8670
Tor020 <kopp020@gmail.com>
2018-03-22 17:25:56 -0400

    'change'

---
+++ src/index.js
@@ -0,0 +1,8 @@
+//
+// PATH OF THIS FILE: ./src/index.js
+//
+import style from "./public/css/style.css";
+
+const arr = [1, 2, 3];
+const iAmJavascriptES6 = () => console.log(...arr);
+window.iAmJavascriptES6 = iAmJavascriptES6;
